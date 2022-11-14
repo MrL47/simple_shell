f38cf3f09061310b04583c721263d8946cc84d66
Kuzivashe <kuzibaye@gmail.com>
2022-11-14 02:45:00 -0800

    alias built-in

--- alias_builtin.c
+++ alias_builtin.c
@@ -65,9 +65,9 @@ void set_alias(char *var_name, cahr *value)
 
 	*value = '\0';
 	value++;
-	len = _strlen(value) - _strspn(value, "'\"");
-	new_value = malloc(sizeof(char) * (len + 1));
-	if(!new_value)
+	length = _strlen(value) - _strspn(value, "'\"");
+	new_value = malloc(sizeof(char) * (length + 1));
+	if (!new_value)
 		return;
 	for (j = 0, k = 0; value[j]; j++)
 	{
@@ -89,20 +89,62 @@ void set_alias(char *var_name, cahr *value)
 		add_alias_end(&aliases, var_name, new_value);
 }
 
+/**
+  * print_alias - prints the alias in the format name = 'value'
+  * @alias: pointer to an alias
+  */
+void print_alias(alias_t *alias)
+{
+	char *alias_string;
+	int length = _strlen(alias->name) + _strlen(alias->value) + 4;
 
+	alias_string = malloc(sizeof(char) * (length + 1));
+	if (!alias_string)
+		return;
+	_strcpy(alias_string, alias->name);
+	_strcat(alias_string, "='");
+	_strcat(alias_string, alias->value);
+	_strcat(alias_string, "'\n");
 
+	write(STDOUT_FILENO, alias_string, len);
+	free(alias_string);
+}
 
+/**
+  * replace_aliases - go through the arguments and replaces any matching
+  * aliases with their value
+  * @args: 2D pointer to the arguments
+  * Return: 2D pointer to the arguments
+  */
+char **replace_aliases(char **args)
+{
+	alias_t *temp;
+	int i;
+	char *new_value;
 
-
-
-
-
-
-
-
-
-
-
-
-
-
+	if (_strcmp(args[0], "alias") == 0)
+		return (args);
+	for (i = 0; args[i]; i++)
+	{
+		temp = aliases;
+		while (temp)
+		{
+			if (_strcmp(args[i], temp->name) == 0)
+			{
+				new_value = malloc(sizeof(char) * (_strlen(temp->value) + 1));
+				if (!new_value)
+				{
+					free_args(args, args);
+					return (NULL);
+				}
+				_strcpy(new_value, temp->value);
+				free(args[i]);
+				args[i] = new_value;
+				i--;
+				break;
+			}
+			temp = temp->next;
+		}
+	}
+	return (args);
+}
